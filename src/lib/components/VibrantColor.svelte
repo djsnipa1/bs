ed5a6b411af73e5e83408051b25dabdd9efbdc27
Chad Boyce <chadboyce@gmail.com>
2024-10-11 17:30:54 -0400

    feat: Add VibrantColor component for dynamic color generation
    - Adding a new function to load a new image and update the palette.
    - Updating the image size and border radius in the VibrantColor component.
    - Improving the styling of the color list in the VibrantColor component.
    
    fix: Update image width to full screen and add margin to color palette in VibrantColor.svelte
    
    style: Add flexbox to VibrantColor component for better spacing between image and palette
    
    feat: Enhance VibrantColor component with improved UI and palette display
    
    VibrantColor finish touches

---
+++ src/lib/components/VibrantColor.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { getPaletteFromImage } from '$lib/util/vibrant';
+  import { onMount } from 'svelte';
+
+  let getRandomNumber = (num) => {
+    return Math.floor(1000 + Math.random() * (num - 1000));
+  };
+
+  let getNewParams = (random) => {
+    const urlParams = new URLSearchParams({
+      fetchUrl: picsumUrl(random)
+    });
+    return urlParams;
+  };
+
+  let picsumUrl = (seed) => {
+    return `https://picsum.photos/seed/${seed}/1200/900/`;
+  };
+
+  let imageUrl = `api/fetch-image?${getNewParams(getRandomNumber(10000))}`;
+
+  let palette = {};
+
+  let loadNewImage = async () => {
+    let newImageUrl = `api/fetch-image?${getNewParams(getRandomNumber(10000))}`;
+    palette = await getPaletteFromImage(newImageUrl);
+    imageUrl = newImageUrl;
+  };
+
+  onMount(async () => {
+    palette = await getPaletteFromImage(imageUrl);
+    // console.log(Object.values(palette));
+  });
+</script>
+
+{#if palette && Object.values(palette)[5]}
+  <div
+    class="flex h-screen w-full flex-col items-center gap-2 font-sofiasans font-extrabold"
+    style={`background-color: ${Object.values(palette)[5].hex};`}
+  >
+    <h1
+      class="pt-8 text-4xl"
+      style={`color: ${Object.values(palette)[1].hex};`}
+    >
+      Vibrant Color
+    </h1>
+
+    <img
+      src={imageUrl}
+      class="rounded-me w-3/4 rounded-md border-8 shadow-md shadow-black/35"
+      style={`border-color: ${Object.values(palette)[0].hex};`}
+      alt="vibrant color photograph"
+    />
+
+    {#if palette}
+      <div class="mt-4 w-1/2 font-normal">
+        <ul>
+          {#each Object.keys(palette) as key}
+            <li
+              class="mb-1.5 rounded-md border p-0.5 pl-2 shadow-sm shadow-black/35 {key.includes(
+                'Light'
+              )
+                ? 'border-gray-800/75 text-gray-800'
+                : 'border-gray-500/75 text-gray-200'}"
+              style="background-color: {palette[key].hex};"
+            >
+              {key}: {palette[key].hex}
+            </li>
+          {/each}
+        </ul>
+      </div>
+      <button class="btn btn-sm mt-4 h-2 pb-2 pt-2" on:click={loadNewImage}
+        >Load new image</button
+      >
+    {/if}
+  </div>
+{:else}
+  <p class="font-sofiasans text-xl font-light text-gray-400">
+    Loading vibrant color...
+  </p>
+{/if}
