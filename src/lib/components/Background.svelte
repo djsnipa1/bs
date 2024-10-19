209dc3e2eb50001565f9879c016a384aa1c80abb
Chad Boyce <chadboyce@gmail.com>
2024-10-19 18:41:06 -0400

    removed gradient Switch button

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -5,7 +5,7 @@
 
   const toLch = useMode(modeLch);
   const toOklch = useMode(modeOklch);
-  // const toOklch = converter('oklch');
+
   let hexColor,
     hexColorDarker,
     hexColorLighter = null;
@@ -83,18 +83,7 @@
   <div
     class="background-gradient absolute z-[-10] min-h-screen min-w-full"
   ></div>
-  <button
-    class="button btn absolute top-80 z-[999999999]"
-    on:click={switchGradient}>Switch</button
-  >
 </div>
-
-<!-- React Div -->
-<!-- <main -->
-<!-- class="absolute z-[999999999] flex min-h-[100dvh] w-full items-center justify-center" -->
-<!-- > -->
-<!-- <button class="button btn" on:click={switchGradient}>Switch</button> -->
-<!-- </main> -->
 
 <style>
   :root {
@@ -103,17 +92,6 @@
     --color3: hsl(213deg 80% 40%);
     --color4: hsl(215deg 80% 29%);
   }
-
-  /* .background-gradient { */
-  /*   background-image: radial-gradient( */
-  /*     circle, */
-  /*     var(--color1) 0%, */
-  /*     var(--color4) 100% */
-  /*   ); */
-  /*   transition: */
-  /*     --color1 0.5s, */
-  /*     --color4 1.6s; */
-  /* } */
 
   .background-gradient {
     background-image: radial-gradient(
