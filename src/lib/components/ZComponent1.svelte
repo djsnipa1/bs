1f9f474424a9593c30b86e45c3cfa9b96139ab1b
Chad Boyce <chadboyce@gmail.com>
2024-11-08 06:04:44 -0500

    feat: Add ZComponent1, ZComponent2, and z route page

---
+++ src/lib/components/ZComponent1.svelte
@@ -0,0 +1,46 @@
+<script>
+  let fillBox = false;
+
+  function handleMouseDown() {
+    fillBox = true;
+  }
+
+  function handleMouseUp() {
+    fillBox = false;
+  }
+</script>
+
+<div
+  class="relative h-screen w-full border-2 border-dotted border-yellow-500 p-2"
+>
+  <div
+    class="{fillBox
+      ? 'bg-pink-300'
+      : ''} flex size-32 items-center justify-center border-2 border-pink-800"
+  >
+    <p class="text-2xl font-bold">1</p>
+  </div>
+
+  <button
+    class="test button btn absolute left-28 top-20 bg-orange-500 text-orange-900 active:bg-orange-600"
+    on:mousedown={handleMouseDown}
+    on:mouseup={handleMouseUp}
+    on:touchstart|preventDefault={handleMouseDown}
+    on:touchend={handleMouseUp}
+    on:mouseleave={handleMouseUp}>CLICK</button
+  >
+
+  <div class="absolute left-8 top-36 font-sofiasans font-thin">
+    <p class="py-2 text-3xl leading-8 text-orange-500">Layer 1 test text...</p>
+    <p class="test py-2 text-xl leading-8 text-amber-400">
+      Shouldn't be selectable...
+    </p>
+  </div>
+</div>
+
+<style>
+  .test {
+    -webkit-user-select: none;
+    user-select: none;
+  }
+</style>
---
+++ src/routes/z/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import ZComponent1 from '$lib/components/ZComponent1.svelte';
+  import ZComponent2 from '$lib/components/ZComponent2.svelte';
+</script>
+
+<div class="relative h-screen w-full">
+  <div class="absolute inset-0 h-screen w-full">
+    <ZComponent1 />
+  </div>
+  <div class="absolute inset-0 h-screen w-full">
+    <ZComponent2 />
+  </div>
+</div>
