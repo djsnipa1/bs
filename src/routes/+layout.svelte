2b683421d7e1257d473688a4b520b0073acca6cc
Chad Boyce <chadboyce@gmail.com>
2024-11-20 14:51:33 -0500

    feat: Add divs route page
    added divs route for testing
    
    divs route is working correctly
    
    got rid of unused css in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,12 +34,4 @@
     /* mobile viewport bug fix */
     min-height: -webkit-fill-available;
   }
-
-  .gradient {
-    background-image: linear-gradient(
-      109.6deg,
-      rgba(62, 161, 219, 1) 11.2%,
-      rgba(93, 52, 236, 1) 100.2%
-    );
-  }
 </style>
---
+++ src/routes/divs/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+  const buttonPositions = [
+    'top-4 left-4', // Div 1: Upper left
+    'top-4 right-4', // Div 2: Upper right
+    'top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2', // Div 3: Center
+    'bottom-20 left-4', // Div 4: Lower left
+    'bottom-20 right-4' // Div 5: Lower right
+  ];
+
+  const colors = [
+    'text-red-500 border-red-500',
+    'text-blue-500 border-blue-500',
+    'text-green-500 border-green-500',
+    'text-yellow-500 border-yellow-500',
+    'text-purple-500 border-purple-500'
+  ];
+
+  function handleClick(index) {
+    alert(`Clicked button ${index + 1}`);
+  }
+</script>
+
+<div class="relative h-screen w-full overflow-hidden">
+  {#each Array(5) as _, i}
+    <div
+      class="pointer-events-none absolute inset-0 flex items-center justify-center"
+      style="z-index: {i};"
+    >
+      <!-- <h2 class="text-4xl font-bold {colors[i].split(' ')[0]}">Div {i + 1}</h2> -->
+      <button
+        class="absolute {buttonPositions[
+          i
+        ]} rounded bg-white px-4 py-2 font-semibold shadow hover:bg-gray-100 {colors[
+          i
+        ]} pointer-events-auto border-2"
+        on:click={() => handleClick(i)}
+      >
+        Button {i + 1}
+      </button>
+    </div>
+  {/each}
+</div>
